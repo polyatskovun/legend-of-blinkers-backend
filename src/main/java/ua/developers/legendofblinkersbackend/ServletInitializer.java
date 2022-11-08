3aa6dd1f94420fd430513ed3843d12d03dd30f27
Polyatskovaya Nastya <anastasia.polyatskovaya@gmail.com>
2022-11-08 14:45:49 +0200

    add javadoc

--- src/main/java/ua/developers/legendofblinkersbackend/ServletInitializer.java
+++ src/main/java/ua/developers/legendofblinkersbackend/ServletInitializer.java
@@ -3,6 +3,9 @@ package ua.developers.legendofblinkersbackend;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * Servlet init, will remove in future commits
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
