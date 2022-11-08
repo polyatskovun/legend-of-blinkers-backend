f88985923853639c199a95455b7978e2959187c2
Polyatskovaya Nastya <anastasia.polyatskovaya@gmail.com>
2022-11-08 14:43:50 +0200

    init commit

---
+++ src/main/java/ua/developers/legendofblinkersbackend/LegendOfBlinkersBackendApplication.java
@@ -0,0 +1,13 @@
+package ua.developers.legendofblinkersbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendOfBlinkersBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegendOfBlinkersBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/developers/legendofblinkersbackend/LegendOfBlinkersBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.developers.legendofblinkersbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegendOfBlinkersBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
